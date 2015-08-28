f192bdebe62d9bfe70d1f52f9ea362cc2258e9e9
mnorlund <michael.norlund@atos.net>
2015-08-28 15:33:42 +0200

    Correction du deploy

--- _install/deploy.sh
+++ _install/deploy.sh
@@ -7,10 +7,15 @@ export PRG_DIR=`(cd ${EXEC_DIR} ; echo $PWD)`
 #PRG_DIR correspond au chemin complet vers _install
 SRCDIR="${PRG_DIR}/.."
 
-usage="## RESTo administration deployment\n\n  Usage $0 -t <ADMIN_TARGET> -b <BACKUPDIR>\n"
-while getopts "t:b:h" options; do
+USER="exppeps"
+GROUP="peps"
+
+usage="## RESTo administration deployment\n\n  Usage $0 -t <ADMIN_TARGET> -u <unix_user> -g <unix group> -b <BACKUPDIR>\n"
+while getopts "t:u:g:b:h" options; do
     case $options in
         t ) TARGETDIR=`echo $OPTARG`;;
+        u ) USER=`echo $OPTARG`;;
+        g ) GROUP=`echo $OPTARG`;;
         b ) BACKUPDIR=`echo $OPTARG`;;
         h ) echo -e $usage;;
         \? ) echo -e $usage
@@ -27,24 +32,34 @@ fi
 
 if [ -d "$TARGETDIR" ]; then
     if [ "$(ls $TARGETDIR)" ]; then
+        # backup
     	if [ ! -d "$BACKUPDIR" ]; then
     		mkdir $BACKUPDIR
 			if [[ $? != 0 ]]; then
 				exit $rc
 			fi	
 		fi
-		
-		tar czf ${BACKUPDIR%%/}/Administration_$(date +"%Y-%m-%d").tgz $TARGETDIR 
+	    BASE=`basename $TARGETDIR`
+	    DIR=`dirname $TARGETDIR`
+	    echo "Creating a backup in $BACKUPDIR"
+	    tar czf ${BACKUPDIR%%/}/admin-$(date +"%Y-%m-%d").tgz -C $DIR $BASE
 		rc=$?
 		if [[ $? != 0 ]]; then
 			exit $rc
 		fi			
-		rm -r $TARGETDIR
+		rm -rf $TARGETDIR
     fi
 fi
 mkdir $TARGETDIR
 
 echo ' ==> Copy files to $TARGETDIR directory'
 cp -Rf $SRCDIR/index.html $SRCDIR/favicon.ico $SRCDIR/app $SRCDIR/assets $TARGETDIR
+
+
+
+echo " ==> Applying unix user's rights."
+chown -R $USER:$GROUP $TARGETDIR
+chmod -R 750 $TARGETDIR
+
 echo ' ==> Successfully install resto administration to $TARGETDIR directory'
 echo ' ==> Now, do not forget to check $TARGETDIR/app/components/app.constant.js configuration !'
