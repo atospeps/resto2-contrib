9b5169b230a795210f7eb86bb011989cdbae07a8
mvican <maxime.vican@atos.net>
2015-12-08 16:25:04 +0100

    Change dashboard stats arbo

--- app/components/app.constant.js
+++ app/components/app.constant.js
@@ -25,7 +25,7 @@
     angular.module('administration')
             .constant('CONFIG', {
                 'restoServerUrl': 'http://192.168.56.101/resto',
-                'acquisitionStatsUrl': 'http://172.24.219.218/acq-rest',
+                'acquisitionStatsUrl': 'http://172.24.219.226/acq-rest',
                 'acquisitionModuleUrl': 'http://172.24.219.218:8080',
                 'administrationEndpoint': '/administration',
                 'offset': 20,
